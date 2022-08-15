bb4fd01959b8d96cfe9b641a9bacd1c8bdb19c99
PavelPoleshchuk <ppl@vivanta.by>
2022-08-15 21:09:28 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,24 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  constructor() {
+  };
+  setRange(min, max) {
+    this.min=min;
+    this.max=max;
+    
+  };
+  
+  guess() {
+    //if (this.medium===number){return number};
+  this.medium=Math.ceil((this.min+this.max)/2);
+    return this.medium;
+  }
+  lower() {
+    this.max=this.medium;
+  }
+  greater() {
+    this.min=this.medium;
+  };
 }
+
 
 module.exports = GuessingGame;
